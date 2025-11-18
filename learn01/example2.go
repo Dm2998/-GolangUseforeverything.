f1eb2a0733e80b0e38693526e0f8e174356e5d8d
Dm2998 <diego2998@gmail.com>
2025-11-18 17:07:46 +0000

    example to continue with Go, Example 2

---
+++ learn01/example2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var steps int = 21 //  string type variable declaration and initialization for number of steps
+
+var msg = "Learning Go is fun!" // string type variable declaration and initialization for message
+
+func main() {
+	fmt.Println(msg, steps, "its great!") // Print message along with steps
+}
