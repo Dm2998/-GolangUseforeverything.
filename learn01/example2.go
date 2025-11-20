b55e2e2bdc8b071910945c92e82538614faf1c70
Dm2998 <diego2998@gmail.com>
2025-11-20 13:44:26 +0000

    initial commit for the example 3

--- learn01/example2.go
+++ learn01/example2.go
@@ -7,5 +7,6 @@ var steps int = 21 //  string type variable declaration and initialization for n
 var msg = "Learning Go is fun!" // string type variable declaration and initialization for message
 
 func main() {
+	
 	fmt.Println(msg, steps, "its great!") // Print message along with steps
 }
---
+++ learn01/example3.go
@@ -0,0 +1,11 @@
+package main 
+
+import "fmt"
+
+var steps int = 30 // string type variable declaration and initialization for number of steps
+
+var msg  = "Learning Go is my plan for now!" // string type variable declaration and initialization for message
+
+func main() {
+	fmt.Println(msg, steps, "its great!") // Print message along with steps
+}
