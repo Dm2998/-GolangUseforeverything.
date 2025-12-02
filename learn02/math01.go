c6adae6ca5fdc06653269f6d2f4ccd2b7aef5157
Dm2998 <diego2998@gmail.com>
2025-12-02 21:42:54 +0000

    Continue with Go

--- learn02/math01.go
+++ learn02/math01.go
@@ -1,1 +1,22 @@
-//    learn
+//	  packge learn02
+
+
+
+package main
+
+import "fmt"
+
+type matrix [2][2][2][2]byte
+
+func main () {
+	var mat1 matrix 
+	mat1 = initMat()
+	fmt.Println(mat1)
+}
+
+func initMat() matrix {
+
+	return matrix {
+		{{{4,4}, {3,5}}, {{55,12}, {22,4}}},
+	}
+}
