afa44f1ef3961ac9944d33656dbd00143e2c97f6
Dm2998 <diego2998@gmail.com>
2025-12-05 00:05:36 +0000

    to update the committed for the learning Golang update same.

--- learn02/slice_make.go
+++ learn02/slice_make.go
@@ -12,9 +12,11 @@ func main() {
 	q1 := months[0:3]
 	q2 := months[3:6]
 	q3 := months[6:9]
+	q4 := months[9:12]
 
 	fmt.Println("First quarter:", len(months), cap(months), months)
 	fmt.Println("Second quarter:", len(q1), cap(q1), q2)
 	fmt.Println("Third quarter:", len(q2), cap(q2), q3)
+	fmt.Println("Fourth quarter:", len(q3), cap(q3), q4)
 
 }
