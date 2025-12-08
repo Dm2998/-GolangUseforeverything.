ba779711948f2633ea8ffd34241a6fb2b1a403f7
Dm2998 <diego2998@gmail.com>
2025-12-08 14:56:52 +0000

    Currencies func for learning.

---
+++ learn03/Currencies.go
@@ -0,0 +1,32 @@
+
+
+packege main
+
+import "fmt"
+
+
+
+
+var currencies = []Curr{
+	Curr{"NOK", "Norwegian Krone", "Norwary", 578},
+	Curr{"KES", "Kenyan Shilling", "Kenya", 404},
+	Curr{"DZD", "Algerian Dinar", "Algeria", 12},
+	Curr{"AUD", "Australian Dollar", "Australia", 36},
+	Curr{"MXN", "Mexican Peso", "Mexico", 484},
+	Curr{"GRD", "Drachma", "Greece", 978},
+	Curr{"KHR", "Riel", "Cambodia", 116},
+	Curr{"SZL", "Lilangeni", "Swaziland", 748},
+	Curr{"GBP", "Pound Sterling", "Isle of Man", 826},
+	Curr{"HTG", "Gourde", "Haiti", 332},
+	Curr{"BWP", "Pula", "Botswana", 72},
+	Curr{"CLP", "Chilean Peso", "Chile", 152},
+	Curr{"HKD", "Hong Kong Dollar", "Hong Koong", 344},
+	Curr{"HTG", "Gourde", "Haiti", 332},
+	Curr{"TRY", "Turkish Lira", "Turkey", 949},
+	Curr{"EUR", "Euro", "Belgium", 978},
+	Curr{"JMD", "Jamaican Dollar", "Jamaica", 388},
+	Curr{"ALL", "Lek", "Albania", 8},
+	Curr{"GEL", "Lari", "Georgia", 981},
+	Curr{"KFM", "Coromo Franc", "Comoros", 174},
+	Curr{"NZD", "New Zeland Dollar", "Tokelau", 554},
+}
