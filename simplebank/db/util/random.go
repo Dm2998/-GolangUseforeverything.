09e97a52fed474197e21278e3d45cce173515cd5
Dm2998 <diego2998@gmail.com>
2025-12-13 16:00:48 +0000

    test the run

--- simplebank/db/util/random.go
+++ simplebank/db/util/random.go
@@ -1,7 +1,9 @@
+// folder util / random.go
 package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -9,23 +11,33 @@ const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-
-
 }
 
-
-func RandomInt(min,max int64) int64 {
-	return min + rand.Int63n(max - min + 1)
-
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
 }
 
 func RandomString(n int) string {
-	var sb string.Builder
+	var sb strings.Builder
 	k := len(alphabet)
 
 	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
-		sb,Write
+		sb.WriteByte(c)
 	}
+	return sb.String()
 }
 
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
