b2ebcc720bf4fb0e6645bdfb826326c33cc2c5db
Dm2998 <diego2998@gmail.com>
2025-12-13 15:08:36 +0000

    Make to new folder with utils more intex the list

--- simplebank/db/util/random.go
+++ simplebank/db/util/random.go
@@ -1,5 +1,31 @@
 package util
 
+import (
+	"math/rand"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 func init() {
-	ran
+	rand.Seed(time.Now().UnixNano())
+
+
 }
+
+
+func RandomInt(min,max int64) int64 {
+	return min + rand.Int63n(max - min + 1)
+
+}
+
+func RandomString(n int) string {
+	var sb string.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb,Write
+	}
+}
+
